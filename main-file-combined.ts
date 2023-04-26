5fe9ea39d1e8790a948fb26049496e8b7cae688f
chdean-09 <124236637+chdean-09@users.noreply.github.com>
2023-04-26 17:20:13 +0800

    Update main-file-combined.ts

--- main-file-combined.ts
+++ main-file-combined.ts
@@ -111,117 +111,7 @@ abstract class healthTracker implements healthTrackerInterface {
   }
 }
 
-class myHealthApp extends healthTracker {
-  constructor(name: string, gender: string, age: number, height: number, weight: number) {
-    super(name, gender, age, height, weight);
-  }
 
-  // this overrides the displayInfo() from the parent class
-  public override displayInfo(): void {
-    console.log("==========HEALTH PROFILE==========");
-    console.log(`Profile: ${this.name}`);
-    console.log(`Gender: ${this.gender}`);
-    console.log(`Age: ${this.age} years old`);
-    console.log(`Height: ${this.height} cm`);
-    console.log(`Weight: ${this.weight} kg`);
-    console.log("--------");
-    console.log(`BMI Profile: ${this.getBMIResult()}`);
-    console.log(`BMR* (Basal Metabolic Rate): ${this.calculateCaloriesBurned().toFixed(0)} calories/day`);
-    console.log("--------");
-    console.log(`Average steps per day: ${this.getAverageSteps()} steps`);
-    console.log(`Average water intake per day: ${this.getAverageWaterIntake().toFixed(1)} ounces`);
-    console.log(`Average calories consumed: ${this.getAverageCalories().toFixed(0)} calories`);
-    console.log(`Average sleep per night: ${this.getAverageSleepDuration().toFixed(1)} hours`);
-    console.log("\n*BMR- calories burned per day just by existing");
-    console.log("==================================");
-  }
-
-  // this overloads the logMeal(food: string, calories: number)
-  // the user can input both the food and calories, but can also choose to just input food
-  public override logMeal(food: string): void;
-  public override logMeal(food: string, calories: number): void;
-  public override logMeal(food: string, calories?: number): void {
-    if (calories !== undefined) {
-      this.food.push(food);
-
-      if (calories >= 0) {
-        this.calories.push(calories);
-      } else {
-        throw new Error("Don't input negative numbers");
-      }
-    } else {
-      this.food.push(food);
-    }
-  }
-
-  private getBMIResult(): string {
-    if (this.getBMI() < 18.5) {
-      return `${this.getBMI().toFixed(1)} (Underweight)`;
-    } else if (this.getBMI() >= 18.5 && this.getBMI() < 24.9) {
-      return `${this.getBMI().toFixed(1)} (Normal weight)`;
-    } else if (this.getBMI() >= 25 && this.getBMI() < 29.9) {
-      return `${this.getBMI().toFixed(1)} (Overweight)`;
-    } else {
-      return `${this.getBMI().toFixed(1)} (Obese)`;
-    }
-  }
-
-  private getAverageSteps(): number {
-    if (this.calories.length === 0) {
-      return 0;
-    } else {
-      let total: number = 0;
-
-      for (let i = 0; i < this.steps.length; i++) {
-        total += this.steps[i];
-      }
-
-      return total / this.steps.length;
-    }
-  }
-
-  private getAverageWaterIntake(): number {
-    if (this.waterIntake.length === 0) {
-      return 0;
-    } else {
-      let total: number = 0;
-
-      for (let i = 0; i < this.waterIntake.length; i++) {
-        total += this.waterIntake[i];
-      }
-
-      return total / this.waterIntake.length;
-    }
-  }
-
-  private getAverageCalories(): number {
-    if (this.calories.length === 0) {
-      return 0;
-    } else {
-      let total: number = 0;
-
-      for (let i = 0; i < this.calories.length; i++) {
-        total += this.calories[i];
-      }
-
-      return total / this.calories.length;
-    }
-  }
-
-  private getAverageSleepDuration(): number {
-    if (this.sleepDuration.length === 0) {
-      return 0;
-    } else {
-      let total: number = 0;
-
-      for (let i = 0; i < this.sleepDuration.length; i++) {
-        total += this.sleepDuration[i];
-      }
-
-      return total / this.sleepDuration.length;
-    }
-  }
-}
 
 
 class nutritionPal extends healthTracker {
@@ -351,25 +241,7 @@ class nutritionPal extends healthTracker {
   }
 }
 
-//name, gender, age, height (in cm), weight (in kg)
-const person1Health: myHealthApp = new myHealthApp("Stanhope Dela Cruz","Male",20,162,74);
-person1Health.logMeal("Rice", 30);
-person1Health.logWaterIntake(6.5);
-person1Health.logMeal("Spaghetti", 221);
-person1Health.logWaterIntake(8.3);
-person1Health.logMeal("Burgers");
-person1Health.logWaterIntake(7.1);
-person1Health.logMeal("Pizza", 187);
-person1Health.logWaterIntake(7.8);
-person1Health.logMeal("Hotdog");
-person1Health.recordStepCount(10019);
-person1Health.recordSleep(4.9);
-person1Health.recordStepCount(463);
-person1Health.recordSleep(9.2);
-person1Health.recordStepCount(870);
-person1Health.recordSleep(7.1);
 
-person1Health.displayInfo();
 
 //food name, calories (in kcal), protein, carbs, fat (in grams)
 const person1Nutrition: nutritionPal = new nutritionPal("Chad Andrada","Male",20,176,63);
