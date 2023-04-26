a1a172cdec88bfe36c3ee930d2b3b72c890edc60
chdean-09 <124236637+chdean-09@users.noreply.github.com>
2023-04-26 17:18:22 +0800

    Update main-file-combined.ts

--- main-file-combined.ts
+++ main-file-combined.ts
@@ -352,7 +352,7 @@ class nutritionPal extends healthTracker {
 }
 
 //name, gender, age, height (in cm), weight (in kg)
-let person1Health: myHealthApp = new myHealthApp("Stanhope Dela Cruz","Male",20,162,74);
+const person1Health: myHealthApp = new myHealthApp("Stanhope Dela Cruz","Male",20,162,74);
 person1Health.logMeal("Rice", 30);
 person1Health.logWaterIntake(6.5);
 person1Health.logMeal("Spaghetti", 221);
@@ -372,7 +372,7 @@ person1Health.recordSleep(7.1);
 person1Health.displayInfo();
 
 //food name, calories (in kcal), protein, carbs, fat (in grams)
-let person1Nutrition: nutritionPal = new nutritionPal("Chad Andrada","Male",20,176,63);
+const person1Nutrition: nutritionPal = new nutritionPal("Chad Andrada","Male",20,176,63);
 person1Nutrition.logMeal("Noodles", 30, 15, 21, 8);
 person1Nutrition.logMeal("Cake", 73, 18, 38, 11);
 person1Nutrition.logMeal("Biscuits", 23, 14, 20, 7);
